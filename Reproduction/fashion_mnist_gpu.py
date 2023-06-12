7acce58c98424a054371944e3fa91e48ad62e0d4
anass.al-ammiri <51008418+RandomAnass@users.noreply.github.com>
2023-06-12 13:17:08 +0200

    Update fashion_mnist_gpu.py

--- Reproduction/fashion_mnist_gpu.py
+++ Reproduction/fashion_mnist_gpu.py
@@ -50,7 +50,7 @@ def circular_padding(x, padding_size):
     # Perform circular padding on the input tensor
     return tf.pad(x, [[0, 0], [padding_size, padding_size], [padding_size, padding_size], [0, 0]], mode='SYMMETRIC')
 
-def simple_Conv(n_hidden, kernel_size=28, padding_size=-1):
+def simple_Conv(n_hidden, kernel_size=10, padding_size=-1):
     if padding_size == -1:
         padding_size = kernel_size // 2
 
@@ -63,7 +63,7 @@ def simple_Conv(n_hidden, kernel_size=28, padding_size=-1):
 
     return model
 
-def simple_Conv_NL(n_hidden,kernel_size=28):
+def simple_Conv_NL(n_hidden,kernel_size=10):
     """ no lambda """
     model = Sequential()
     model.add(Conv2D(n_hidden, kernel_size=kernel_size, padding='same', input_shape=(28, 28, 1), activation='relu'))
@@ -72,10 +72,11 @@ def simple_Conv_NL(n_hidden,kernel_size=28):
 
     return model
 
-def simple_FC(n_hidden):
+def simple_FC(n_hidden, n_units = 2):
     model = Sequential()
     model.add(Flatten(input_shape=(28, 28)))
-    model.add(Dense(n_hidden, activation="relu"))
+    for i in range(n_units):
+        model.add(Dense(n_hidden, activation="relu"))
     model.add(Dense(10))
 
     return model
@@ -134,9 +135,9 @@ def simple__RNN():
 
 print("creating models")
 model_names = {'simple_FC':simple_FC, 'simple_Conv':simple_Conv, 'simple_Conv_NL':simple_Conv_NL,
-               'simple_Conv_max':simple_Conv_max, 'simple__RNN':simple__RNN , 'simple_FC_2':simple_FC_2, 'simple_Conv_2':simple_Conv_2}
+               'simple_Conv_max':simple_Conv_max, 'simple_FC_':simple_FC, 'simple_Conv_':simple_Conv,'simple__RNN':simple__RNN , 'simple_FC_2':simple_FC_2, 'simple_Conv_2':simple_Conv_2}
 
-padding_size = 0 #padding_sizes[3]
+padding_size = 5 #padding_sizes[3]
 n_hidden = 1024 #1000
 kernel_size=28
 
@@ -149,13 +150,11 @@ def train_step(model, optimizer, loss_object, images, labels):
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return loss
 
-def create_art_classifier(model_creator, x_train, y_train, batch_size=200, nb_epochs=30, **kwargs): # nb_epochs=200
-    # Create the CNN model and optimizer
+def create_art_classifier(model_creator, x_train, y_train, x_test, y_test, batch_size=200, nb_epochs=20, **kwargs):
     model = model_creator(**kwargs)
-    optimizer = tf.keras.optimizers.legacy.SGD(learning_rate=0.1, momentum=0.9, decay=5e-4)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
     loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
     model.compile(optimizer=optimizer, loss=loss_object, metrics=['accuracy'])
-
 
     # Create the ART classifier
     classifier = TensorFlowV2Classifier(
@@ -166,58 +165,75 @@ def create_art_classifier(model_creator, x_train, y_train, batch_size=200, nb_ep
         input_shape=(28, 28, 1),
         clip_values=(0, 1),
     )
-    # Custom training loop
+
     train_acc = []
     test_acc = []
+    train_loss = []
+    test_loss = []
+
     for epoch in range(nb_epochs):
         print("Epoch {}/{}".format(epoch + 1, nb_epochs))
         epoch_loss = []
+
+        if epoch == 10 or epoch == 15:
+            optimizer.learning_rate.assign(optimizer.learning_rate * 0.1)
+
         for batch in range(0, len(x_train), batch_size):
             batch_images = x_train[batch:batch + batch_size]
             batch_labels = y_train[batch:batch + batch_size]
             loss = train_step(model, optimizer, loss_object, batch_images, batch_labels)
             epoch_loss.append(loss)
-        #print(epoch_loss)
-        avg_loss = np.mean(epoch_loss)
-        print("Average loss: {:.4f}".format(avg_loss))
 
-        # Perform predictions and evaluate accuracy on examples
+        avg_loss = np.mean(epoch_loss)
+        train_loss.append(avg_loss)
+        print("Average training loss: {:.4f}".format(avg_loss))
+
+        # evaluate accuracy on test examples
         predictions = classifier.predict(x_test)
+        test_loss_value = loss_object(y_test, predictions).numpy()
+        test_loss.append(test_loss_value)
         accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
         test_acc.append(accuracy)
         print("Accuracy on test examples: {:.2%}".format(accuracy))
 
+        # evaluate accuracy on train examples
         predictions = classifier.predict(x_train)
         accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_train, axis=1)) / len(y_train)
         train_acc.append(accuracy)
         print("Accuracy on train examples: {:.2%}".format(accuracy))
 
-    return [classifier, test_acc, train_acc]
+    return [classifier, test_acc, train_acc, test_loss, train_loss]
+
 
 models = [
-    create_art_classifier(model_creator=simple_FC, x_train=x_train, y_train=y_train, n_hidden=n_hidden),
-    create_art_classifier(model_creator=simple_Conv, x_train=x_train, y_train=y_train, n_hidden=n_hidden, kernel_size=kernel_size, padding_size=padding_size),
-    create_art_classifier(model_creator=simple_Conv_NL, x_train=x_train, y_train=y_train, n_hidden=n_hidden, kernel_size=kernel_size),
-    create_art_classifier(model_creator=simple_Conv_max, x_train=x_train, y_train=y_train, n_hidden=n_hidden, kernel_size=kernel_size),
+    create_art_classifier(model_creator=simple_FC, x_train=x_train, y_train=y_train, n_hidden=256,n_units=2),
+    create_art_classifier(model_creator=simple_Conv, x_train=x_train, y_train=y_train, n_hidden=512, kernel_size=10, padding_size=padding_size),
+    create_art_classifier(model_creator=simple_Conv_NL, x_train=x_train, y_train=y_train, n_hidden=512, kernel_size=10),
+    create_art_classifier(model_creator=simple_Conv_max, x_train=x_train, y_train=y_train, n_hidden=512, kernel_size=10),
+    create_art_classifier(model_creator=simple_FC, x_train=x_train, y_train=y_train, n_hidden=512,n_units=3),
+    create_art_classifier(model_creator=simple_Conv, x_train=x_train, y_train=y_train, n_hidden=2048, kernel_size=12, padding_size=padding_size),
     create_art_classifier(model_creator=simple__RNN, x_train=x_train, y_train=y_train),
     create_art_classifier(model_creator=simple_FC_2, x_train=x_train, y_train=y_train),
     create_art_classifier(model_creator=simple_Conv_2, x_train=x_train, y_train=y_train)
 ]
+print("saving the models")
 for i, model in enumerate(models):
     model_name = f'model_{i}.h5'
     model[0].model.save(model_name)
 
 array_models = np.array(models)
 acc_array = array_models[:, [1,2]]
-
+loss_array = array_models[:, [3,4]]
 
 # Save the models list to a file
 with open('acc_array.pkl', 'wb') as file:
     pickle.dump(acc_array, file)
-
+with open('loss_array.pkl', 'wb') as file:
+    pickle.dump(loss_array, file)
 with open('full_np.pkl', 'wb') as file:
     pickle.dump(array_models, file)
 print("done saving models")
+
 # Enable GPU acceleration for ART attacks
 from art.config import ART_NUMPY_DTYPE
 os.environ["ART_NUMPY_DTYPE"] = str(ART_NUMPY_DTYPE)
@@ -229,6 +245,15 @@ os.makedirs(save_dir, exist_ok=True)
 print("creating attack data")
 for model, model_name in zip(models, model_names.keys()):
     classifier = model[0]
+    loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    classifier = TensorFlowV2Classifier(
+        model=classifier,
+        loss_object=loss_object,
+        #train_step=train_step,
+        nb_classes=10,
+        input_shape=(28, 28, 1),
+        clip_values=(0, 1),
+    )
     # Iterate over the attack parameters and generate adversarial examples
     for norm, epsilons in attack_params:
         for epsilon in epsilons:
