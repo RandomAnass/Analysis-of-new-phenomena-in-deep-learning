944dca44eab2aed4dc5ac9907fe1da5197e11936
anass.al-ammiri <51008418+RandomAnass@users.noreply.github.com>
2023-06-17 22:36:16 +0200

    Update full_mnist_gpu_2.py

--- Reproduction/full_mnist_gpu_2.py
+++ Reproduction/full_mnist_gpu_2.py
@@ -72,15 +72,14 @@ def simple_Conv_max(n_hidden, kernel_size=10,padding_size=0):
     return model
 
 def simple_Conv_2():
-    model = tf.keras.Sequential([
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(10)
-    ])
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(10))
 
     return model
 
